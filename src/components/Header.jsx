57774985df0b4dd657fa52e000303c526f74e928
java80 <iekedede@hotmail.com>
2021-03-29 14:28:23 -0400

    Nav bar styling done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
       
-      <div>
+      <div className="navbar">
         <ul>
            <li> <Link to='/'>Home</Link> </li> 
           <li><Link to='/new'>All Cateogry</Link> </li>
           <li><Link to='/new'>Promotion</Link> </li>
           <li><Link to='/new'>New Releases</Link> </li>
-          <li class="shopping-cart"><img src="shop.png" width="50" height="50"></img> </li>
+        <li class="shopping-cart"><img src="shop.png" width="50" height="50"></img> </li>
+        <li><Link to='/new'>Add New Product</Link> </li>
         </ul>
         
 
