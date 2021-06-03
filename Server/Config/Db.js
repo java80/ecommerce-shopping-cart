b252bbc943225bf0aa9bce19945e712135458214
java80 <iekedede@hotmail.com>
2021-06-03 02:22:24 -0400

    Db .js file

---
+++ Server/Config/Db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://new-ike:Cart1234@cluster0.w6r7u.mongodb.net/shoppingcartdatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,22 @@
-const 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//DB Configuration
+const db = require("./Config/Db").mongoURI;
+
+mongoose
+  .connect(db, { usedNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+app.get("/", (req, res) => res.send("Hello ike"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server running on port ${port}`));
