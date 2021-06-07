fe28306850d14d75f68757ae2ee6db7d3aa00338
java80 <iekedede@hotmail.com>
2021-06-06 20:35:26 -0400

    modified ProductBackEndlist

--- src/components/ProductBackEndList.jsx
+++ src/components/ProductBackEndList.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import ReactDOM from 'react-dom'
 import "./Product.css";
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import React from "react";
 function ProductBackEndList(props) {
   let product = props.currentProduct.fields ? props.currentProduct.fields : {};
 
   return (
-
-    <div id='product-list'>
-      <img src={product.imgurl1} />
-      <Link to={`/product/${props.currentProduct.id}`}> <h1>{product.name}</h1>  </Link>
+    <div id="product-list">
+      <img src={product.imgurl1} alt=" product " />
+      <Link to={`/product/${props.currentProduct.id}`}>
+        {" "}
+        <h1>{product.name}</h1>{" "}
+      </Link>
 
       <Link to={`/edit/${props.currentProduct.id}`}>
         <button>Edit</button>
       </Link>
     </div>
-
-  )
-
+  );
 }
 
 export default ProductBackEndList;
